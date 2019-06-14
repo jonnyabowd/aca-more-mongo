d8ce6005996e5b8d3d40e266aae6d6b512f632f9
JL Abowd <jl.abowd@Cheese.local>
2019-06-13 21:04:15 -0500

    IT'S WORKING. IT'S REALLY WORKING

--- express/routes/tickets.js
+++ express/routes/tickets.js
@@ -4,17 +4,38 @@ const TicketsController = require('../controllers/tickets')
 
 // Create a route for getting all movies/tickets from the db
 // This corresponds to item 1 in the controller
-
+router.get('/', (request, response) => {
+    TicketsController.getAll()
+        .then(results => response.json(results))
+})
 
 // Create a route for creating a movie
 // This corresponds to item 2 in the controller
+router.post('/', (request, response)=> {
+    TicketsController.createMovie(request.body)
+        .then(movie => response.json(movie))
+})
 
+// covers use case if user sends empty delete call
+router.delete('/', (request, response) => {
+    response.json({ err: 'fat idiot'})
+})
 
 // Create a route for deleting ONE movie by it's name
 // This corresponds to item 3 in the controller
-
+router.delete('/:name', (request, response) => {
+    TicketsController.deleteMovie(request.params.name)
+        .then(() => response.json({status: 'Movie deleted'}))
+        .catch((err) => {
+            console.log('BIG ERROR', err.message)
+        })
+})
 
 // Create a route for getting ONE movie by it's id
 // This corresponds to item 4 in the controller
+router.get('/:id', (request, response) => {
+    TicketsController.getById(request.params.id)
+        .then((movie) => response.json(movie))
+})
 
 module.exports = router
